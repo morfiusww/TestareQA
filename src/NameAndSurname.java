1f421b39cc47e7bcd13eae5177b26640730d29ea
Morfius <heies228@gmail.com>
2023-05-18 20:06:51 +0300

    Initial commit

---
+++ src/NameAndSurname.java
@@ -0,0 +1,9 @@
+public class NameAndSurname {
+    public static void main(String[] args) {
+        String name = "Roman";
+        String surname = "Ciorici";
+        String function = "Card Manager";
+        System.out.println("My Name is " + name + " " + surname);
+        System.out.print("My Function is " + " " + function);
+    }
+}
